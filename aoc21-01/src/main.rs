e817797050d0804998e42b85b0b20cd626db70e0
Maurits Lam <mhjlam@users.noreply.github.com>
2021-12-06 19:10:15 +0100

    Day 1

---
+++ aoc21-01/src/main.rs
@@ -0,0 +1,37 @@
+fn part1() {
+    let input = include_str!("input.txt");
+    let mut prev = std::i32::MAX;
+
+    let matches = input.lines()
+    .map(|x| x.parse::<i32>().unwrap())
+    .filter(|x| {
+        let cond = x > &prev;
+        prev = *x;
+        return cond;
+    });
+
+    println!("{}", matches.count());
+}
+
+fn part2() {
+    let input = include_str!("input.txt").lines().map(|x| x.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+
+    let mut prev_sum = 0;
+    let mut increases = 0;
+
+    for i in 2..input.len() {
+        let sum = input[i - 2 as usize] + input[i - 1 as usize] + input[i];
+
+        if prev_sum > 0 && sum > prev_sum {
+            increases += 1;
+        }
+        prev_sum = sum;
+    }
+
+    println!("{}", increases);
+}
+
+fn main() {
+    part1();
+    part2();
+}
